08c293049bd47a4c4dd885326813eee4b8b316d0
seo <seo@172.22.111.214>
2018-06-12 17:42:52 +0900

    Updatd calender main()

--- src/calender/calender.java
+++ src/calender/calender.java
@@ -5,5 +5,12 @@ public class calender {
 	public static void main(String[] args) {
 		
 		System.out.println("hello calender");
+		System.out.println("월 화 수 목 금 토");
+		System.out.println(" 1 2 3 4 5 6 7");
+		System.out.println(" 1 2 3 4 5 6 7");
+		System.out.println(" 1 2 3 4 5 6 7");
+		System.out.println(" 1 2 3 4 5 6 7");
+	
+	
 	}
 }
