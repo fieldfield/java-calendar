24d845d034a1beedeeb9312c7f79b9de0c6584a7
seo <seo@172.22.111.214>
2018-06-13 10:19:52 +0900

    update using scanner

--- src/calender/calender.java
+++ src/calender/calender.java
@@ -1,16 +1,29 @@
 package calender;
 
+import java.util.Scanner;
+
 public class calender {
 
 	public static void main(String[] args) {
 		
-		System.out.println("hello calender");
-		System.out.println("월 화 수 목 금 토");
-		System.out.println(" 1 2 3 4 5 6 7");
-		System.out.println(" 1 2 3 4 5 6 7");
-		System.out.println(" 1 2 3 4 5 6 7");
-		System.out.println(" 1 2 3 4 5 6 7");
+//		System.out.println("hello calender");
+//		System.out.println("월 화 수 목 금 토");
+//		System.out.println(" 1 2 3 4 5 6 7");
+//		System.out.println(" 1 2 3 4 5 6 7");
+//		System.out.println(" 1 2 3 4 5 6 7");
+//	   	System.out.println(" 1 2 3 4 5 6 7");
 	
-	
+		Scanner scanner = new Scanner(System.in);
+		
+		int[] month = {31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
+		
+		int a;
+		
+		System.out.println("숫자를 입력해주세요");
+		a = scanner.nextInt();
+		
+		System.out.println(a+ "은 " + month[a-1]+"일입니다 "  );
+		
+		scanner.close();
 	}
 }
