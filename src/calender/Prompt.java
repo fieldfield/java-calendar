27ab90f3a891894984db8d48d2d14a3a7974963c
seo <seo@218.38.137.27>
2018-06-15 14:19:42 +0900

    editing calender

--- src/calender/Prompt.java
+++ src/calender/Prompt.java
@@ -2,32 +2,54 @@ package calender;
 
 import java.util.Scanner;
 
+
 public class Prompt {
 
-	private final static String PROMPT = "Cal>";
-
+/**	
+ * 
+ * @param week 요일
+ * @return 0~6 sun~sat
+ */
+	
+	public int parseDay(String week) {
+		if(week.equals("su")) return 0;
+		else if(week.equals("mo")) return 1;
+		else if(week.equals("tu")) return 2;
+		else if(week.equals("we")) return 3;
+		else if(week.equals("th")) return 4;
+		else if(week.equals("fr")) return 5;
+		else if(week.equals("sa")) return 6;
+		else 
+			return 0;
+	}
+	
 	public void rumPrompt() {
 		Scanner scanner = new Scanner(System.in);
 		calender cal = new calender();
 		
-		int month = -1;
-		int year = -1;
+		int month = 1;
+		int year = 2017;
+		int weekday = 0;
 		
 		while(true) {
-			System.out.println("년도 입력해주세요");
+			System.out.println("년도 입력해주세요(exit : -1)");
 			System.out.print("Year>>");
 			year = scanner.nextInt();			
-	
+			if(year == -1)
+				break;
+			
 			System.out.println("달을 입력해주세요");
 			System.out.print("Month>>");
 			month = scanner.nextInt();			
-			if(month == -1) {
-				break;
-			}
-			if (month > 12) {
-		 		continue;
+			System.out.println("첫째 날의 요일을 입력하세요.(su, mo, tu, we, th, fr, sa) ");
+			String  str_weekday = scanner.next();
+			weekday = parseDay(str_weekday);
+			
+			if (month > 12 || month < 1) {
+		 		System.out.println("잘못된 입력입니다");
+				continue;
 			}				
-			cal.printCalender(year, month);
+			cal.printCalender(year, month, weekday);
 		}
 		System.out.println("bye~");		
 		scanner.close();
--- src/calender/calender.java
+++ src/calender/calender.java
@@ -1,7 +1,5 @@
 package calender;
 
-import java.util.Scanner;
-
 public class calender {
 
 	private final int[] MAX_DAYS = {31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
@@ -23,33 +21,46 @@ public class calender {
 	}
 	}
 	
-
-	/*
+/*
+	
 	- 월을 입력하면 해당월의 달력을 출력한다.
 	- 달력의 모양은 1단계에서 작성한 모양으로 한다.
 	- 1일은 일요일로 정해도 무방하다.
 	- -1을 입력받기 전까지 반복 입력한다.
 	- 프롬프트를 출력한다.
-	*/
+	
+	- 1일의 요일을 입력받는다.
+ 	- 출력한다.
+*/	
 	
 	
-	public void printCalender(int year, int month) {
+	public void printCalender(int year, int month, int weekday) {
 		System.out.printf("<<%4d년%3d월>>\n", year, month);
 		System.out.println("su mo tu  we  th fr sa");
-
-		int maxDay = getMaxDaysOfMonth(year, month);
 		
-		for(int i =1; i <= maxDay; i++) {
+		//print blank space
+		for(int i=0; i< weekday; i++) {
+			System.out.print("   ");
+		}
+		
+		int maxDay = getMaxDaysOfMonth(year, month);
+		int count = 7- weekday;
+		int delim = (count< 7) ? count : 0;
+		
+		//print fist line
+		for(int i =1; i <= count; i++) {
 			System.out.printf("%3d",i);
-			if(i % 7 == 0) {
+		}
+		System.out.println();
+		
+		for(int i = count+1; i <= maxDay; i++) {
+			System.out.printf("%3d",i);
+			if(i % 7 ==  delim) {
 				System.out.println();
 			}
 		}
 		System.out.println();
-//		System.out.println(" 1 2 3 4 5 6 7");
-//		System.out.println(" 1 2 3 4 5 6 7");
-//		System.out.println(" 1 2 3 4 5 6 7");
-//	   	System.out.println(" 1 2 3 4 5 6 7");
+		System.out.println();
 	}
 	
 }
