ce903251d2246e9dc433f529ef77fa1170fb2969
seo <seo@218.38.137.27>
2018-06-15 12:08:56 +0900

    update repeat calender

--- src/calender/calender.java
+++ src/calender/calender.java
@@ -4,26 +4,38 @@ import java.util.Scanner;
 
 public class calender {
 
+	private final int[] MAX_DAYS = {31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
+	
+	public int getMaxDaysOfMonth(int month) {
+		return MAX_DAYS[month-1];
+	}
+	public void printSampleCalender() {
+		System.out.println("hello calender");
+		System.out.println("월 화 수 목 금 토");
+		System.out.println(" 1 2 3 4 5 6 7");
+		System.out.println(" 1 2 3 4 5 6 7");
+		System.out.println(" 1 2 3 4 5 6 7");
+	   	System.out.println(" 1 2 3 4 5 6 7");
+	}
 	public static void main(String[] args) {
 		
-//		System.out.println("hello calender");
-//		System.out.println("월 화 수 목 금 토");
-//		System.out.println(" 1 2 3 4 5 6 7");
-//		System.out.println(" 1 2 3 4 5 6 7");
-//		System.out.println(" 1 2 3 4 5 6 7");
-//	   	System.out.println(" 1 2 3 4 5 6 7");
 	
+		String PROMPT = "cal>"; 
 		Scanner scanner = new Scanner(System.in);
+		calender cal = new calender();
 		
-		int[] month = {31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
+		System.out.println("반복횟수를 입력하세요.");
 		
-		int a;
+		int repeat = scanner.nextInt();
 		
-		System.out.println("숫자를 입력해주세요");
-		a = scanner.nextInt();
-		
-		System.out.println(a+ "은 " + month[a-1]+"일입니다 "  );
-		
-		scanner.close();
+		for(int i=0; i< repeat; i++) {
+			System.out.println("달을 입력해주세요");
+			System.out.print(PROMPT);
+
+			int month = scanner.nextInt();			
+			System.out.printf("%d월은 %d일까지 있습니다.\n",month, cal.getMaxDaysOfMonth(month));
+		}
+		System.out.println("bye~");		
+				scanner.close();
 	}
 }
